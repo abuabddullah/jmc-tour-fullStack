51feb183cc4aacefee8710d3bf064466d19458f7
asifaowadud <asifaowadud@gmail.com>
2024-08-28 10:25:40 +0600

    nothing to write

--- src/app/(client)/locations/[_id]/page.jsx
+++ src/app/(client)/locations/[_id]/page.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // This function also runs at build time
-async function getLocationData(id) {
-  const res = await fetch(`/api/locations/location/${id}`);
-  const data = await res.json();
-  const { location } = data;
-  return location;
-}
 
 export default async function LocationPage({ params }) {
+  const [location, setLocation] = useState({});
   const { _id } = params;
-  const location = await getLocationData(_id);
+  useEffect(() => {
+    async function getLocationData(id) {
+      const res = await fetch(`/api/locations/location/${id}`);
+      const data = await res.json();
+      const { location } = data;
+      return location;
+    }
+    setLocation(getLocationData(_id));
+  }, [_id]);
 
   return <div>page: {location ? location._id : "not found"}</div>;
 }
