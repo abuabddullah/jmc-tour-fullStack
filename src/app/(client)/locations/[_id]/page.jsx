6e7d20d5d03391469149d410ff17e15328f1f343
asifaowadud <asifaowadud@gmail.com>
2024-08-28 10:18:28 +0600

    nothing to write

--- src/app/(client)/locations/[_id]/page.jsx
+++ src/app/(client)/locations/[_id]/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // This function also runs at build time
 async function getLocationData(id) {
-  const res = await fetch(`http://localhost:3000/api/locations/location/${id}`);
+  const res = await fetch(`/api/locations/location/${id}`);
   const data = await res.json();
   const { location } = data;
   return location;
